13b636f67965559651ee0a266c24abaa027e9155
evgenyoborotistov <evgeny.oborotistov@weare.fi>
2019-06-17 21:19:24 +0300

    Apply type interface to mongoose model and move gitignore up

--- Backend/lib/models/productModel.ts
+++ Backend/lib/models/productModel.ts
@@ -1,37 +1,47 @@
-import * as mongoose from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
+
+interface IProduct extends Document {
+    amazonId: string;
+    category: string;
+    name: string;
+    brand: string;
+    images: any[];
+    description: string;
+    created_date: Date;
+}
 
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  amazonId: {
-    type: String,
-    required: 'You must specify an Amazon ID for new product'
-  },
-  category: {
-    type: String,
-    required: 'You must specify a category for new product'
-  },
-  name: {
-    type: String,
-    required: 'You must specify a name for new product'
-  },
-  brand: {
-    type: String,
-    required: 'You must specify a brand for new product'
-  },
-  // TODO think about price
-  images: {
-    type: Array
-  },
-  description: {
-    type: String
-  },
-  created_date: {
-    type: Date,
-    default: Date.now
-  }
+    amazonId: {
+        type: String,
+        required: "You must specify an Amazon ID for new product"
+    },
+    category: {
+        type: String,
+        required: "You must specify a category for new product"
+    },
+    name: {
+        type: String,
+        required: "You must specify a name for new product"
+    },
+    brand: {
+        type: String,
+        required: "You must specify a brand for new product"
+    },
+    // TODO think about price
+    images: {
+        type: Array
+    },
+    description: {
+        type: String
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-const Product = mongoose.model('Product', ProductSchema);
+const Product = mongoose.model<IProduct>("Product", ProductSchema);
 
 export default Product;
